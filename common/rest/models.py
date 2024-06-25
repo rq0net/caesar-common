9c2407136d85f371c3f7f5b806dfaee3b1bcd788
vrymel <vrymel.omandam@asians.cloud>
2024-06-25 11:26:19 +0800

    added TGUser

--- common/rest/models.py
+++ common/rest/models.py
@@ -2,9 +2,18 @@ from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
+from django.db import models
 
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
         Token.objects.create(user=instance)
+
+
+class TGUser(models.Model):
+    name = models.TextField(unique=True, blank=True, null=True)
+    value = models.TextField(default="", null=False)
+
+    def __str__(self):
+        return str(self.name)
