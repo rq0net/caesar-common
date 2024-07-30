833a29cadfed9823f722125054f95b1fdc23088c
I Komang Suryadana <suryadana80@gmail.com>
2024-07-30 14:35:25 +0800

    Revert "implement audit logs in caesar-common"

--- common/config/common.py
+++ common/config/common.py
@@ -148,7 +148,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "common.rest.middleware.changeLog.ChangeLogMiddleware",
 ]
 
 ROOT_URLCONF = '%s.urls' % APP_NAME.lower()
@@ -408,5 +407,3 @@ INSTALLED_APPS += [
     'zone',
 ]
 
-ELASTICSEARCH_API_HOST=os.getenv('ELASTICSEARCH_API_HOST','')
-ELASTICSEARCH_API_KEY=os.getenv('ELASTICSEARCH_API_KEY','')
